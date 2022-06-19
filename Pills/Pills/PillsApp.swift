d2466e097ec3136f21ac8521f44aee8a3b00e4cf
BrzozowskiPawel <pawelbrzozowskigplus@gmail.com>
2022-06-19 11:08:35 +0200

    Created a project

---
+++ Pills/Pills/PillsApp.swift
@@ -0,0 +1,17 @@
+//
+//  PillsApp.swift
+//  Pills
+//
+//  Created by Paweł Brzozowski on 19/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct PillsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
